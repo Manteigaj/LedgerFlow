9a0cc9f097c0dbfbd0e565fd8668da87a734ff45
João Gabriel Monteiro <joaog1734@gmail.com>
2026-08-26 17:48:30 -0300

    fix Docker setup and database migrations

--- alembic/versions/f18170cca6eb_rename_categories_table.py
+++ alembic/versions/f18170cca6eb_rename_categories_table.py
@@ -6,8 +6,6 @@ Revises: d45149f81386
 
 from typing import Sequence, Union
 
-from alembic import op
-
 
 revision: str = "f18170cca6eb"
 down_revision: Union[str, Sequence[str], None] = "d45149f81386"
@@ -16,24 +14,10 @@ depends_on: Union[str, Sequence[str], None] = None
 
 
 def upgrade() -> None:
-    """Rename categories table and column."""
-
-    op.rename_table("categorias", "categories")
-
-    op.alter_column(
-        "categories",
-        "nome",
-        new_column_name="name",
-    )
+    """No schema changes required."""
+    pass
 
 
 def downgrade() -> None:
-    """Restore categories table and column."""
-
-    op.alter_column(
-        "categories",
-        "name",
-        new_column_name="nome",
-    )
-
-    op.rename_table("categories", "categorias")
+    """No schema changes required."""
+    pass
